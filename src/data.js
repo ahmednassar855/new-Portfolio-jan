d835d65698ba3926fab388f2eb5998fbd8a622fb
ahmednassar855 <75637929+ahmednassar855@users.noreply.github.com>
2023-01-18 18:29:35 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,88 +2,92 @@ const currentDate = new Date();
 
 export const myProfile = [
 
-    {
-        id: 1,
-        company: "AZFC (Abo Zaabal for Chemical and Fertilizer Company)",
-        title: "Chemist Shift Engineer",
-        details: "controlling operations from storage sulfur rock then to fusion sulfur, filtration, burning in furnace then to boiler to cooling then converting by catalytic converter Vanadium dioxide then to demister tower to convert it to sulfuric acid with 98 % concentration & finally to storage & shipping tanks.? Reverse osmosis unit / demineralization water treatment unit.? In addition water treatment for processing water and boiling water that is used for cooling the boiler and heat Exchangers? Controlling the operations through DCS. ",
-        startDate: 2008,
-        endDate: 2010,
-        till: 2010,
-        duration: 2,
-    },
-    {
-        id: 2,
-        company: "Apache / Khalda - Petroleum Company",
-        title: "Lab Chemist",
-        details: "Managing oil lab activities for oil and water analysis and managing oil field chemical treatment, such as:? Demulsifier / Deoiler evaluation testing to improve water separation, water and oil quality.? Paraffin control study (PPD) to improve pour point degree in shipping crude oil and ? paraffin dispersant and inhibitor control.? H2S scavenger chemical evaluation for gas / liquid phase, Drag reducer chemical  evaluation, Involved in scale and corrosion chemicals control.? Controlling / monitoring injection over field locations.? Injection / sampling point’s location selection, Monitoring separation performance in oil facilities, Daily wells monitoring, collecting oil, water, gas and solid samples.? Provide justification for the increase or drop in production.? Involved in purchasing and evaluation the technical offers for lab devices, tools and lab chemicals.",
-        startDate: 2010,
-        endDate: 2016,
-        till: 2016,
-        duration: 4,
-    },
+  {
+    id: 1,
+    company: "AZFC (Abo Zaabal for Chemical and Fertilizer Company)",
+    title: "Chemist Shift Engineer",
+    details: "controlling operations from storage sulfur rock then to fusion sulfur, filtration, burning in furnace then to boiler to cooling then converting by catalytic converter Vanadium dioxide then to demister tower to convert it to sulfuric acid with 98 % concentration & finally to storage & shipping tanks.? Reverse osmosis unit / demineralization water treatment unit.? In addition water treatment for processing water and boiling water that is used for cooling the boiler and heat Exchangers? Controlling the operations through DCS. ",
+    startDate: 2008,
+    endDate: 2010,
+    till: 2010,
+    duration: 2,
+  },
+  {
+    id: 2,
+    company: "Apache / Khalda - Petroleum Company",
+    title: "Lab Chemist",
+    details: "Managing oil lab activities for oil and water analysis and managing oil field chemical treatment, such as:? Demulsifier / Deoiler evaluation testing to improve water separation, water and oil quality.? Paraffin control study (PPD) to improve pour point degree in shipping crude oil and ? paraffin dispersant and inhibitor control.? H2S scavenger chemical evaluation for gas / liquid phase, Drag reducer chemical  evaluation, Involved in scale and corrosion chemicals control.? Controlling / monitoring injection over field locations.? Injection / sampling point’s location selection, Monitoring separation performance in oil facilities, Daily wells monitoring, collecting oil, water, gas and solid samples.? Provide justification for the increase or drop in production.? Involved in purchasing and evaluation the technical offers for lab devices, tools and lab chemicals.",
+    startDate: 2010,
+    endDate: 2016,
+    till: 2016,
+    duration: 4,
+  },
 
-    {
-        id: 3,
-        company: "Apache / Khalda - Petroleum Company",
-        title: "Chemist - Asset Integirty ",
-        details: "  Technical holder / managing oil field chemicals ? Managing chemical stock & inventory / prepare ordering plan / orders issuing, review and tracking chemical cost and treatment cost.? Technical decision / advisor for chemical selection, monitoring and its evaluation.? Controlling the consumption, injection, stock, efficiency and its evaluation for the following chemicals (Pour point depressant, paraffin dispersant, paraffin inhibitors, demulsifier, deoiler, Drag reducer, H2S scavenger, Scale inhibitors, corrosion inhibitors, antifoulant , Biocide )? Contractors chemist/ technical holder:Technical team leader / holder for chemicals tendering, preparing tendering plan, prepare tender scope of work. Assist in selecting potential contractors, carrying out bid evaluations on a commercial basis, technical evaluation. ? Review chemicals KPI",
-        startDate: 2016,
-        endDate: 2020,
-        till: 2020,
-        duration: 4,
-    },
-    {
-        id: 4,
-        company: "Apache / Khalda - Petroleum Company",
-        title: "Chemist - Supply Chain Project Purchasing ",
-        details: " ",
-        startDate: 2021,
-        endDate: currentDate.getFullYear(),
-        till: "up to date",
-        duration: 2,
-    }
+  {
+    id: 3,
+    company: "Apache / Khalda - Petroleum Company",
+    title: "Chemist - Asset Integirty ",
+    details: "  Technical holder / managing oil field chemicals ? Managing chemical stock & inventory / prepare ordering plan / orders issuing, review and tracking chemical cost and treatment cost.? Technical decision / advisor for chemical selection, monitoring and its evaluation.? Controlling the consumption, injection, stock, efficiency and its evaluation for the following chemicals (Pour point depressant, paraffin dispersant, paraffin inhibitors, demulsifier, deoiler, Drag reducer, H2S scavenger, Scale inhibitors, corrosion inhibitors, antifoulant , Biocide )? Contractors chemist/ technical holder:Technical team leader / holder for chemicals tendering, preparing tendering plan, prepare tender scope of work. Assist in selecting potential contractors, carrying out bid evaluations on a commercial basis, technical evaluation. ? Review chemicals KPI",
+    startDate: 2016,
+    endDate: 2020,
+    till: 2020,
+    duration: 4,
+  },
+  {
+    id: 4,
+    company: "Apache / Khalda - Petroleum Company",
+    title: "Chemist - Supply Chain Project Purchasing ",
+    details: " ",
+    startDate: 2021,
+    endDate: currentDate.getFullYear(),
+    till: "up to date",
+    duration: 2,
+  }
 ]
 
 export const courseCertifcate = [
-    { id : 1 , src : "assets/URN_00693616  _IG.png" , title : "NEBOSH IGC"},
-    { id : 2 , src : "assets/Full Stack Development Track Udacity.png" , title : "Full stack Udacity"},
-    { id : 3 , src : "assets/Udacity Nanodegree Graduation Certificate.png"  , title : "Udacity Nanodegree"},
-    { id : 4 , src : "assets/Tailwind certificate from mindluster platform.png" , title : "Tailwind mindluster"}
+  { id: 1, src: "assets/URN_00693616  _IG.png", title: "NEBOSH IGC" },
+  { id: 2, src: "assets/Full Stack Development Track Udacity.png", title: "Full stack Udacity" },
+  { id: 3, src: "assets/Udacity Nanodegree Graduation Certificate.png", title: "Udacity Nanodegree" },
+  { id: 4, src: "assets/Tailwind certificate from mindluster platform.png", title: "Tailwind mindluster" }
 ];
 
 
 export const projectData = [
-    {
-      src: "assets/proj-hummty.png",
-      title: "Food App",
-      siteLink: "https://ahmednassar855.github.io/hummtyDummty/",
-      gitHubLink: "https://ahmednassar855.github.io/hummtyDummty/",
-      main: "React Js",
-      details: "using react js , api food , css stying and bootstrap",
-    },
-    {
-      src: "assets/wather-app.png",
-      title: "Weather app",
-      siteLink: "https://ahmednassar855.github.io/weather-app-Task05/",
-      gitHubLink: "https://ahmednassar855.github.io/weather-app-Task05/@github",
-      main: "Vanilla Js",
-      details: "javaScript pure , weather food , css stying and bootstrap",
-    },
-    {
-      src: "assets/egyptPArty01.png",
-      title: " Egypt party",
-      siteLink: "https://ahmednassar855.github.io/egyptianParty-JS06/",
-      gitHubLink: "https://ahmednassar855.github.io/egyptianParty-JS06/@github",
-      main: "Vanilla Js",
-      details: "javaScript pure + weather food",
-    },
-    {
-      src: "assets/loginsystem01.png",
-      title: " Login System",
-      siteLink: "https://ahmednassar855.github.io/loginSystemTask-04/",
-      gitHubLink: "https://ahmednassar855.github.io/loginSystemTask-04/@github",
-      main: "Vanilla Js",
-      details: "javaScript pure , localstorag  , css styling and bootstrap",
-    },
-  ];
+  {
+    src: "assets/proj-hummty.png",
+    title: "Food App",
+    siteLink: "https://ahmednassar855.github.io/hummtyDummty/",
+    gitHubLink: "https://ahmednassar855.github.io/hummtyDummty/",
+    main: "React Js",
+    details: "using react js , api food , css stying and bootstrap",
+  },
+  {
+    src: "assets/wather-app.png",
+    title: "Weather app",
+    siteLink: "https://ahmednassar855.github.io/weather-app-Task05/",
+    gitHubLink: "https://ahmednassar855.github.io/weather-app-Task05/@github",
+    main: "Vanilla Js",
+    details: "javaScript pure , weather food , css stying and bootstrap",
+  },
+  {
+    src: "assets/egyptPArty01.png",
+    title: " Egypt party",
+    siteLink: "https://ahmednassar855.github.io/egyptianParty-JS06/",
+    gitHubLink: "https://ahmednassar855.github.io/egyptianParty-JS06/@github",
+    main: "Vanilla Js",
+    details: "javaScript pure + weather food",
+  },
+  {
+    src: "assets/loginsystem01.png",
+    title: " Login System",
+    siteLink: "https://ahmednassar855.github.io/loginSystemTask-04/",
+    gitHubLink: "https://ahmednassar855.github.io/loginSystemTask-04/@github",
+    main: "Vanilla Js",
+    details: "javaScript pure , localstorag  , css styling and bootstrap",
+  },
+];
+
+export const progileImage = [
+ "assets/myLogo.png",
+];
