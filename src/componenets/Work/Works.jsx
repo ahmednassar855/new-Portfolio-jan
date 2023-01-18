e3168725dd9684d9980fca576d7274c30000d3b9
ahmednassar855 <75637929+ahmednassar855@users.noreply.github.com>
2023-01-18 18:35:09 +0200

    Update Works.jsx

--- src/componenets/Work/Works.jsx
+++ src/componenets/Work/Works.jsx
@@ -4,51 +4,6 @@ import WorkItem from "../Work/WorkItem";
 import { SwitchThemeContext } from "./../../Context/SwitchThemeStore";
 import { myProfile } from './../../data';
 
-// const data = [
-//   {
-//     year: 2020,
-//     title: " developer",
-//     duration: " 3 years",
-//     startDate: 2017,
-//     endDate: 2020,
-//     company: "cccccc",
-//     details:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe earum ut harum ipsam laboriosam ab amet soluta nam dolore quos? ",
-//   },
-//   {
-//     year: 2020,
-//     title: "Front End developer",
-//     duration: " 3 years",
-//     startDate: 2017,
-//     endDate: 2020,
-//     company: "cccccc",
-//     details:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe earum ut harum ipsam laboriosam ab amet soluta nam dolore quos? ",
-//   },
-//   {
-//     year: 2017,
-//     title: " Ui developer",
-//     duration: " 2 years",
-//     startDate: 2016,
-//     endDate: 2017,
-//     company: "cccccc",
-//     details:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe earum ut harum ipsam laboriosam ab amet soluta nam dolore quos? ",
-//   },
-//   {
-//     year: 2016,
-//     title: "ux designer",
-//     duration: " 1 years",
-//     startDate: 2015,
-//     endDate: 2016,
-//     company: "cccccc",
-//     details:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe earum ut harum ipsam laboriosam ab amet soluta nam dolore quos? ",
-//   },
-// ];
-
-
-
 const Works = () => {
   let { switchTheme } = useContext(SwitchThemeContext);
 
@@ -85,17 +40,8 @@ const Works = () => {
         ))}
       </div>
 
-      <div className="group relative  h-[600px]  border-2 border-stone-200 w-1/2 m-auto mt-5 flex items-center  justify-center">
+      <div className="h-[600px]  border-2 border-stone-200  w-full lg:w-1/2 m-auto mt-5">
         <CourseSwiper />
-        <div className="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-stone-700/60 group-hover:h-full group-hover:opacity-100 duration-500 ease-in group-hover:z-50 overflow-hidden">
-        
-          <button
-            class="mt-5 px-8 py-3 rounded-full bg-amber-400 hover:bg-amber-600 duration-300"
-            href="#"
-          >
-            More Info.
-          </button>
-        </div>
       </div>
     </div>
   );
